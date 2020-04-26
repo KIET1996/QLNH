959b537f84f77a46e50c5661fa825a963ea2e629
huavoanhthu <huavoanhthu@users.noreply.github.com>
2020-04-27 02:45:44 +0700

    edit giao dien table

--- QLNH/QLNH/frmTables.cs
+++ QLNH/QLNH/frmTables.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLNH.Controller;
+using QLNH.Model;
 
 namespace QLNH
 {
     public partial class frmTables : Form
     {
+
+        BindingSource positionList = new BindingSource(); //khai bao bindingsource de moi lan load du lieu se thay doi theo binding
+        BindingSource tableList = new BindingSource();
         public frmTables()
         {
             InitializeComponent();
@@ -23,5 +28,32 @@ namespace QLNH
             frmOther frm = new frmOther();
             frm.Show();
         }
+
+        private void frmTables_Load(object sender, EventArgs e)
+        {
+            //load du lieu position
+            position_Load();
+            dtGridPosition.DataSource = positionList;
+
+            //load du lieu table
+            table_Load();
+            dtGridTable.DataSource = tableList;
+
+        }
+
+        /*-------------------Tao cac ham xu ly Table---------------------*/
+        private void table_Load()
+        {
+            tableList.DataSource = Controller.TableController.Instance.ListTable();
+        }
+        
+
+        /*--------------------Tao cac ham xu ly position------------------*/
+        private void position_Load()
+        {
+            positionList.DataSource = Controller.PositionController.Instance.getListPosition();
+        }
+
+
     }
 }
