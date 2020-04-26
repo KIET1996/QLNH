afc53cd8898a5ded869f3bd2250938c957c13c1a
huavoanhthu <huavoanhthu@users.noreply.github.com>
2020-04-26 23:07:18 +0700

    add PositionController and Position model

---
+++ QLNH/QLNH/Model/Position.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNH.Model
+{
+    class Position
+    {
+        private int IDPosition;
+        private string position;
+        private string status;
+        private string note;
+
+        public Position(int id_pos, string pos, string sta, string note)
+        {
+            this.ID_Position = id_pos;
+            this.NamePosition = pos;
+            this.Status = sta;
+            this.Note = note;
+        }
+
+        public int ID_Position
+        {
+            get
+            {
+                return ID_Position;
+            }
+            set
+            {
+                ID_Position = value;
+            }
+        }
+        public string NamePosition
+        {
+            get
+            {
+                return NamePosition;
+            }
+            set
+            {
+                NamePosition = value;
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return Status;
+            }
+            set
+            {
+                Status = value;
+            }
+        }
+        public string Note
+        {
+            get
+            {
+                return Note;
+            }
+            set
+            {
+                Note = value;
+            }
+        }
+    }
+}
